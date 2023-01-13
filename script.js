e58e76055c88e938bb82941271897d5a5ab5bf45
abmetheny <abmetheny@gmail.com>
2023-01-12 20:03:26 -0500

    Cleaned up code, removed console.logs, and added comments to js code

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ window.onload = () => {
   const specialCharacters = " \\!#$%&'()*+,-./:;<=>?@[]^_`{|}~\"";
   var selectedCharacters = "";
 
+  // User chooses how long to make the password.
   const chooseLength = prompt("Choose a password length (must be between 8-128 characters).");
   if (chooseLength < 8) {
-    alert("Password must be at least 8 characters.");
+    alert("Password must be at least 8 characters.  Refresh the page and start again.");
   }
   else if (chooseLength > 128) {
-    alert("Password must be less than 128 characters.");
+    alert("Password must be less than 128 characters.  Refresh the page and start again.");
   }
+  // User selects which type of characters to include.
   else {
     const confirmLower = confirm("Include lowercase letters?");
       if (confirmLower == true) {
@@ -31,56 +33,29 @@ window.onload = () => {
         selectedCharacters += specialCharacters;
       }
       if ((confirmLower == false) && (confirmUpper == false) && (confirmNumber == false) && (confirmCharacter == false)) {
-        alert("Password must contain at least one character type.");
+        alert("Password must contain at least one character type.  Refresh the page and start again.");
       }
+      alert("Password ready.  Click the \"Generate Password\" button to display.");
   }
 
-  console.log(selectedCharacters);
-  
+  // Randomly selects characters from the subset of user-defined character types.
   function generatePassword() {
     var randomIndex = "";
     for (var i = 0; i < chooseLength; i++) {
       randomIndex += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
-      console.log(randomIndex);
     } 
     return randomIndex;
   }
 
-
-  var password = generatePassword();
-  console.log(password);
-  // alert("New password: "+ password);
-
-    // Write password to the #password input
+  // Writes password to the #password input
   function writePassword() {
-    const password = generatePassword();
-    const passwordText = document.querySelector("#password");
-    
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
 
-// Add event listener to generate button
+// Adds event listener to generate button
 const generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-
-  
-
-
-
-
 }
-
-
-// // Assignment Code
-
-// function generatePassword() {
-//   const randomValue = Math.floor(Math.random() * upperArray.length);
-
-//   return randomValue;
-// }
-
-
-// const charactersArray = Array.from(upperCharacters + lowerCharacters + numberCharacters + specialCharacters);
-// console.log (charactersArray);
-
